7c22e65d8ff2a37ca82326cc19acf2e22724b83a
Parupally Vamshidhar Reddy <42996478+vamshidhar199@users.noreply.github.com>
2022-02-07 22:50:36 -0800

    add button download

--- src/Component/CenterMenu/content/centercontent.js
+++ src/Component/CenterMenu/content/centercontent.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./centercontent.css";
 import background from "./../../../Assets/background.jpg";
+import { SocialIcon } from "react-social-icons";
+import myResume from "./../../../Assets/Resume-VamshidharReddyParupally.pdf";
 class CenterContent extends Component {
   render() {
     return (
@@ -9,6 +11,21 @@ class CenterContent extends Component {
           <img className="centercontent-div-img" src={background}></img>
           <img className="centercontent-div-h1-profileImg"></img>
           <h1 className="centercontent-div-h1">Vamshidhar Reddy Parupally</h1>
+          <div className="centercontent-div-div">
+            <SocialIcon
+              url="https://linkedin.com/in/vamshidhar-reddy-parupally-550080170"
+              style={{ height: 25, width: 25 }}
+            ></SocialIcon>
+            <a
+              href={myResume}
+              download="VamshidharReddyParupallyResume"
+              target="_blank"
+            >
+              <button className="centercontent-div-div-button">
+                DOWNLOAD CV
+              </button>
+            </a>
+          </div>
         </div>
       </>
     );
